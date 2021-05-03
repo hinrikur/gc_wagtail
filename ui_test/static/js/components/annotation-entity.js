63f5008fefe3b27c32a22ccd65f43dd0789e13fd
hinrikur <hinrik.hafst@gmail.com>
2021-05-03 16:51:20 +0000

    new popover with icons

--- ui_test/static/js/components/annotation-entity.js
+++ ui_test/static/js/components/annotation-entity.js
@@ -1,158 +1,202 @@
 
 const React = window.React;
 const Modifier = window.DraftJS.Modifier;
+const DraftUtils = window.Draftail.DraftUtils;
+
+// const dangerouslySetInnerHTML = window.React.dangerouslySetInnerHTML()
 
 
 
 const Portal = require('./portal.js');
 const AnnotationPopover = require('./popover.js');
-  
+const IconButton = require('./icon-button.js');
+
+var entityMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;'
+};
+
+function escapeHtml(string) {
+  return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+    return entityMap[s];
+  });
+}
+
+function createMarkup(txt) {
+  return { __html: txt };
+}
+
+function formatAnnotation(txt) {
+  // Hack to convert all text within single quotation marks in
+  // an annotation to bold, while also escaping the annotation
+  // text to valid HTML
+  var fmt = txt.replace(/'[^']*'/g, function (s) {
+    // Be careful to not use characters that will be HTML-escaped
+    // in the dummy markers
+    return "[bold]" + s.slice(1, -1) + "[~bold]";
+  });
+  fmt = escapeHtml(fmt);
+  // Replace the dummy markers with HTML tags
+  replaced = fmt.replace(/\[bold\]/g, "<b>").replace(/\[~bold\]/g, "</b>");
+  // Returned as React element
+  return React.createElement("div", {
+    // className: "ann-popover-contents", // incorrect placement of this className
+    dangerouslySetInnerHTML: createMarkup(replaced)
+  });
+}
+
 class AnnotationEntity extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showTooltipAt: null
-        };
-        this.openTooltip = this.openTooltip.bind(this);
-        this.closeTooltip = this.closeTooltip.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      showTooltipAt: null
+    };
+    this.openTooltip = this.openTooltip.bind(this);
+    this.closeTooltip = this.closeTooltip.bind(this);
+  }
+  /* :: openTooltip: (e: Event) => void; */
+
+  openTooltip(e) {
+    const trigger = e.target;
+
+    if (trigger instanceof Element) {
+      this.setState({
+        showTooltipAt: trigger.getBoundingClientRect()
+      });
     }
-    /* :: openTooltip: (e: Event) => void; */
+  }
+  /* :: closeTooltip: () => void; */
 
-    openTooltip(e) {
-        const trigger = e.target;
+  closeTooltip() {
+    this.setState({
+      showTooltipAt: null
+    });
+  }
 
-        if (trigger instanceof Element) {
-            this.setState({
-                showTooltipAt: trigger.getBoundingClientRect()
-            });
-        }
-    }
-    /* :: closeTooltip: () => void; */
+  render() {
+    const {
+      editorState,
+      entityKey,
+      contentState,
+      children,
+      onEdit,
+      onRemove,
+      icon,
+      label,
+      data
+    } = this.props;
 
-    closeTooltip() {
-        this.setState({
-            showTooltipAt: null
-        });
-    }
-
-    render() {
-        const {
-            editorState,
-            entityKey,
-            contentState,
-            children,
-            onEdit,
-            onRemove,
-            icon,
-            label,
-            data
-        } = this.props;
-
-        // const currentContentState = editorState.getCurrentContent();
-
-        // console.log("Editor State from within entity:", currentContentState)
-
-        const { showTooltipAt } = this.state;
-        const { url } = contentState.getEntity(entityKey).getData(); // Contrary to what JSX A11Y says, this should be a button but it shouldn't be focusable.
-        const errCode = data.code;
-        const errSuggestion = data.suggest;
-        const errMessage = data.text;
-
-        //   const newContentState = Modifier.replaceText(currentContent, selectionState, selectedText, null, annotationEntityKey);
-        // onReplace = () => {
-        //     console.log(`replacing word with "${this.props.data.suggest}"`);
-        //   }
-
-        return /*#__PURE__*/ React.createElement(
-            "ann",
+    const { showTooltipAt } = this.state;
+    // 'ann' element, conteins the annotation 
+    return React.createElement(
+      "ann",
+      {
+        role: "button",
+        onMouseUp: this.openTooltip,
+        className: "ann"
+      },
+      React.createElement(
+        "span",
+        {
+          className: "TooltipEntity__text"
+        },
+        children
+      ),
+      showTooltipAt &&
+      // Portal for popover/tooltip
+      React.createElement(
+        Portal,
+        {
+          onClose: this.closeTooltip,
+          closeOnClick: true,
+          closeOnType: true,
+          closeOnResize: true
+        },
+        // AnnotationPopover component
+        React.createElement(
+          AnnotationPopover,
+          {
+            target: showTooltipAt,
+            direction: "top"
+          },
+          // div for popover contents
+          React.createElement(
+            "div",
             {
-                role: "button",
-                onMouseUp: this.openTooltip,
-                className: "ann"
+              className: "ann-popover-contents"
             },
-          // /*#__PURE__*/ React.createElement(Icon, {
-          //   icon: icon,
-          //   className: "TooltipEntity__icon"
-          // }),
-          /*#__PURE__*/ React.createElement(
-                "span",
-                {
-                    className: "TooltipEntity__text"
-                },
-                children
+            // div for annoatation text message
+            React.createElement(
+              "div",
+              {
+                className: "ann-text"
+              },
+              formatAnnotation(this.props.data.text)
             ),
-            showTooltipAt &&
-            /*#__PURE__*/ React.createElement(
-                Portal,
+
+            // div for annotation detail if present
+            typeof this.props.data.detail !== "undefined" && this.props.data.detail !== null
+              ? React.createElement(
+                "div",
                 {
-                    onClose: this.closeTooltip,
-                    closeOnClick: true,
-                    closeOnType: true,
-                    closeOnResize: true
+                  className: "ann-detail"
                 },
-              /*#__PURE__*/ React.createElement(
-                    AnnotationPopover,
-                    {
-                        target: showTooltipAt,
-                        direction: "top"
-                    },
-                    //     /*#__PURE__*/ React.createElement(
-                    //   "a",
-                    //   {
-                    //     href: url,
-                    //     title: url,
-                    //     target: "_blank",
-                    //     rel: "noopener noreferrer",
-                    //     className: "Tooltip__link"
-                    //   },
-                    //   this.props.data.code
-                    // ),
-                    React.createElement(
-                        "div",
-                        {
+                formatAnnotation(this.props.data.detail)
+              )
+              : null
+          ),
 
-                        },
-                        //   React.createElement(
-                        //       "h2",
-                        //       {
-                        //           className: "AnnotationClassLabel",
-                        //       },
-                        //       this.props.data.code
-                        // "Hello World!"
-                        //   ),
-                        React.createElement(
-                            "h3",
-                            {
-                                className: "AnnotationMessage"
-                            },
-                            this.props.data.text
-                        )
-                    ),
+          React.createElement(
+            IconButton,
+            {
+              name: "yes",
+              // active,
+              label: "Samþykkja",
+              title: "Samþykkja uppástungu",
+              icon: "glyphicon glyphicon-ok normal",
+              onClick: onEdit.bind(null, entityKey),
+              // onClick: onRemove.bind(this.props.data.suggest, entityKey),
+            }
+            // "button",
+            // {
+            //     type: "button",
+            //     className: "Annotation__button_yes",
+            //     //   onClick: onEdit.bind(null, entityKey)
+            //     onClick: onRemove.bind(null, entityKey),
+            //     icon: "check"
+            // },
+            // "Samþykkja"
+          ),
+          React.createElement(
+            IconButton,
+            {
+              name: "no",
+              // active,
+              label: "Hafna",
+              title: "Hafna uppástungu",
+              icon: "glyphicon glyphicon-remove normal",
+              onClick: onRemove.bind(null, entityKey),
+            }
+          ),
+          //     React.createElement(
+          // "button",
+          // {
+          //   type: "button",
+          //   className: "Annotation__button_no",
+          //   onClick: onRemove.bind(null, entityKey)
+          // },
+          // "Hafna"
+        )
+      )
 
-                /*#__PURE__*/ React.createElement(
-                        "button",
-                        {
-                            type: "button",
-                            className: "Annotation__button_yes",
-                            //   onClick: onEdit.bind(null, entityKey)
-                            onClick: onRemove.bind(null, entityKey),
-                            icon: "check"
-                        },
-                        "Samþykkja"
-                    ),
-                /*#__PURE__*/ React.createElement(
-                        "button",
-                        {
-                            type: "button",
-                            className: "Annotation__button_no",
-                            onClick: onRemove.bind(null, entityKey)
-                        },
-                        "Hafna"
-                    )
-                )
-            )
-        );
-    }
+    );
+  }
 }
 
 module.exports = AnnotationEntity;
---
+++ ui_test/static/js/components/icon-button.js
@@ -0,0 +1,77 @@
+
+const React = window.React;
+const Icon = window.Draftail.Icon;
+
+class IconButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showTooltipOnHover: true
+    };
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+  /* :: onMouseDown: (e: Event) => void; */
+
+  onMouseDown(e) {
+    const { name, onClick } = this.props;
+    e.preventDefault();
+    this.setState({
+      showTooltipOnHover: false
+    });
+
+    if (onClick) {
+      onClick(name || "");
+    }
+  }
+  /* :: onMouseLeave: () => void; */
+
+  onMouseLeave() {
+    this.setState({
+      showTooltipOnHover: true
+    });
+  }
+
+  render() {
+    const { name, 
+        active, 
+        label, 
+        title, 
+        icon,
+        onClick,
+    } = this.props;
+    const { showTooltipOnHover } = this.state;
+    return /*#__PURE__*/ React.createElement(
+      "button",
+      {
+        name: name,
+        className: `Annotation__button_${name}${
+          active ? " Annotation__button_yes--active" : ""
+        }`,
+        type: "button",
+        "aria-label": title || null,
+        "data-draftail-balloon": title && showTooltipOnHover ? true : null,
+        tabIndex: -1,
+        onClick: onClick
+        // onMouseDown: this.onMouseDown,
+        // onMouseLeave: this.onMouseLeave
+      },
+      typeof icon !== "undefined" && icon !== null
+        ? /*#__PURE__*/ React.createElement("span", {
+            className: icon
+          })
+        : null,
+      label
+        ? /*#__PURE__*/ React.createElement(
+            "span",
+            {
+              className: "Annotation__button_yes__label"
+            },
+            label
+          )
+        : null
+    );
+  }
+}
+
+module.exports = IconButton;
