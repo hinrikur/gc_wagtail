c0be0776b6b8b0e1cd80f621b7522315194e15f6
hinrikur <hinrik.hafst@gmail.com>
2021-05-27 23:30:06 +0000

    Added API call (not implemented)

--- ui_test/static/js/annotate.js
+++ ui_test/static/js/annotate.js
@@ -17,9 +17,39 @@ const convertToRaw = window.DraftJS.convertToRaw;
 const AnnotationEntity = require('./components/annotation-entity.js');
 
 // const processResponce = require('./components/processAPI.js');
-const dummyApi = require('./components/dummyAPI.json');
+const dummyApi = require('./components/correctedReference.json');
 
-console.log(dummyApi)
+// console.log(dummyApi)
+
+// Example POST method implementation:
+async function callGreynirAPI(url = '', data = {}) {
+    if (data === "") {
+        // text is field is empty, returns null
+        return null;
+    } else {
+        console.log("Data being sent via request");
+        console.log(JSON.stringify(data));
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            // mode: 'no-cors', // no-cors, *cors, same-origin
+            // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            // credentials: 'omit', // include, *same-origin, omit
+            scheme: "https",
+            headers: {
+                // 'Content-Type': 'application/json; charset=UTF-8',
+                'Content-Type': 'text/plain',
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                // 'Access-Control-Allow-Origin': 'http://localhost:8000' //'*', 
+            },
+            // redirect: 'follow', // manual, *follow, error
+            // referrerPolicy: 'same-origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: data // body data type must match "Content-Type" header
+        });
+
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+}
 
 function processAPI(json) {
     // iterates over API response JSON and returns flat
